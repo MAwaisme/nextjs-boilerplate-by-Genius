c1415b3e81d293c052431ddc0534cb39f34cfb17
Muhammad Awais <muhammad.awais.genius@gmail.com>
2025-07-26 01:20:16 +0500

    wokring on apis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Web3AuthComp from "@/components/Web3AuthComp";
 export default function Home() {
   return (
     <>
-      <h1>My APP</h1>
       <Web3AuthComp />
       {/* // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
